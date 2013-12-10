8b4f2cdfbc5401f7b3d849d8513dfc08ed9d57e8
Hagen Rother <hagen.rother@liquidm.com>
2013-12-10 14:36:01 +0100

    major refactor/cleanup of dumbo.conf

--- dumbo.rb
+++ dumbo.rb
@@ -10,21 +10,28 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'i18n/core_ext/hash'
 
-def render(template, data_source, counter_name, conf, intervals, files)
+def render(template, data_source, conf, intervals, files)
   template.result(binding)
 end
 
 base_dir = File.dirname(__FILE__)
-conf_name = File.join(base_dir, 'dumbo.conf')
-conf = YAML::load_file(conf_name).deep_symbolize_keys
+
+conf_file = File.join(base_dir, 'dumbo.conf')
+unless File.exist?(conf_file)
+  conf_file = "/etc/druid/dumbo.conf"
+end
+puts "Reading conf from #{conf_file}"
+conf = YAML::load_file(conf_file).deep_symbolize_keys
 
 template_file = File.join(base_dir, 'importer.template')
+unless File.exist?(template_file)
+  template_file = "/etc/druid/importer.template"
+end
+puts "Reading template from #{conf_file}"
 template = ERB.new(IO.read(template_file))
 
-druid = Druid::Client.new(conf[:zookeeper_uri], conf[:druid_options])
-
 def scan_hdfs(path, delta)
-  results = Hash.new{|hash, key| hash[key] = {files: [], events: 0} }
+  results = Hash.new{|hash, key| hash[key] = {files: [], counter: 0} }
   now = Time.now
   allowed_delta = delta.days
 
@@ -45,7 +52,7 @@ def scan_hdfs(path, delta)
       if now - target_time < allowed_delta
         result = results[target_time]
         result[:files] << fullname
-        result[:events] += event_count
+        result[:counter] += event_count
       end
     end
   end
@@ -54,26 +61,49 @@ def scan_hdfs(path, delta)
 end
 
 conf[:db].each do |db_name, options|
-  puts "Scanning #{db_name}"
+  #db_name as symbol sucks
+  db_name = db_name.to_s
+
+  # these params override defaults
+  options[:zookeeper_uri] ||= conf[:default][:zookeeper_uri]  ||= "localhost:2181"
+  options[:metrics]       ||= conf[:default][:metrics]        ||= {}
+  options[:dimensions]    ||= conf[:default][:dimensions]     ||= []
+
+  # these params augment defaults
+  [
+    :druid_client,
+    :raw_input,
+    :segment_output,
+    :database,
+  ].each do |option_group|
+    (options[option_group] ||= {}).merge!(conf[:default][option_group] || {})
+  end
+
+  puts "Scanning #{db_name} on #{options[:zookeeper_uri]}"
+  druid = Druid::Client.new(options[:zookeeper_uri], options[:druid_client])
+
+  # this is what we want to populate
   intervals = []
   files = []
 
-  hdfs_content = scan_hdfs(options[:input][:path], conf[:check_window_days])
+  # scan for camus files
+  hdfs_content = scan_hdfs(options[:raw_input][:hdfs_path], options[:raw_input][:check_window_days])
 
-  hdfs_intervals = hdfs_content.keys.sort[1...-1].map{|check_time| [check_time, check_time + 1.hour]} # skip first and last hour 
+  # skip first and last hour as they are usually incomplete
+  hdfs_intervals = hdfs_content.keys.sort[1...-1].map{|check_time| [check_time, check_time + 1.hour]}
 
   begin
-    query = druid.query(db_name.to_s)
+    query = druid.query(db_name)
               .time_series
-              .long_sum(conf[:counter_name])
+              .long_sum(options[:segment_output][:counter_name])
               .granularity(:hour)
               .intervals(hdfs_intervals)
 
     delta = query.send.each do |druid_numbers|
+      druid_count = druid_numbers[options[:segment_output][:counter_name]]
       delta_time = DateTime.parse(druid_numbers.timestamp)
 
-      hdfs_count  = hdfs_content[delta_time][:events]
-      druid_count = druid_numbers[conf[:counter_name]]
+      hdfs_count  = hdfs_content[delta_time][:counter]
 
       if hdfs_count != druid_count
         puts "Detected delta of #{hdfs_count - druid_count} events for #{db_name} at #{delta_time}"
@@ -82,7 +112,7 @@ conf[:db].each do |db_name, options|
       end
     end
   rescue => e
-    if conf[:allow_full_rescan]
+    if options[:raw_input][:allow_full_rescan]
       puts "Doing a full rescan for #{db_name} as it doesn't seem to exist"
       intervals = hdfs_intervals
       files = hdfs_content.values.map{|c| c[:files]}.flatten
@@ -91,19 +121,18 @@ conf[:db].each do |db_name, options|
     end
   end
 
-  conf_file = File.join(base_dir, "#{db_name.to_s.sub('/', '_')}.druid")
+  job_file = File.join(base_dir, "#{db_name.sub('/', '_')}.druid")
 
   if intervals.length > 0
-    IO.write(conf_file, render(
+    IO.write(job_file, render(
       template,
-      db_name.to_s.split('/')[-1],
-      conf[:counter_name],
+      db_name.split('/')[-1],
       options,
       intervals,
       files
     ))
   else
     puts "Looks like #{db_name} is correct :)"
-    File.delete(conf_file) if File.exist?(conf_file)
+    File.delete(job_file) if File.exist?(job_file)
   end
 end
