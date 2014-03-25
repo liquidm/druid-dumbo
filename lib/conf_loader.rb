99a9044e41c7006c90b92757ec7af09bb0b2b4f0
Hagen Rother <hagen.rother@liquidm.com>
2014-03-25 14:40:26 +0100

    move seed from code to dumbo.config

--- lib/conf_loader.rb
+++ lib/conf_loader.rb
@@ -3,20 +3,6 @@ require 'i18n/core_ext/hash'
 require 'active_support'
 require 'active_support/core_ext'
 
-LIQUIDM = {
-  offset: 1372543200.0,
-  seed: %w{
-    hdfs:///metrik/mysql.seed
-  },
-  legacy: %w{
-    hdfs:///events
-    hdfs:///history/ed_reports
-  },
-  camus: %w{
-    /history/ed_bid_requests
-    /history/ed_events
-  },
-}
 
 class Time
   def floor(granularity = 1.hour)
@@ -37,17 +23,22 @@ def load_config
   puts "Reading conf from #{conf_file}"
   raw_conf = YAML::load_file(conf_file).deep_symbolize_keys
 
-  config = {}
+  config = {
+  }
 
   raw_conf[:db].each do |db_name, options|
     #db_name as symbol sucks
     db_name = db_name.to_s
 
-    # these params override defaults
-    options[:zookeeper_uri] ||= raw_conf[:default][:zookeeper_uri]  ||= "localhost:2181"
-    options[:metrics]       ||= raw_conf[:default][:metrics]        ||= {}
-    options[:dimensions]    ||= raw_conf[:default][:dimensions]     ||= []
-    options[:reschema]      ||= raw_conf[:default][:reschema]       ||= {}
+    {
+      dimensions: [],
+      metrics: {},
+      reschema: {},
+      seed: raw_conf[:seed] || {},
+      zookeeper_uri: "localhost:2181"
+    }.each do |override_option, default_value|
+      options[override_option] ||= (raw_conf[:default][override_option] || default_value)
+    end
 
     # reschema overrides defaults *and* is rewritten
     reschema_raw = options[:reschema] || raw_conf[:default][:reschema] || {}
@@ -74,13 +65,10 @@ def load_config
           data_set[:start_time] = Time.at((now - data_set[:offset]).floor)
           data_set[:end_time] = (now - (reschema[pos + 1])[:offset]).floor
         else
-          data_set[:start_time] = Time.at(LIQUIDM[:offset])
+          data_set[:start_time] = Time.at(options[:seed][:offset])
           data_set[:end_time] = Time.at((now - data_set[:offset]).floor)
-          data_set.merge(LIQUIDM)
         end
       end
-      LIQUIDM.delete :offset
-      options[:seed] = LIQUIDM
     end
 
     # write it back as an array sorted by offset
