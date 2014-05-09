048061adaa289aaef091af298898205a06758ed9
Hagen Rother <hagen.rother@liquidm.com>
2014-05-09 22:54:43 +0200

    revert overdoing lag detection

--- dumbo.rb
+++ dumbo.rb
@@ -68,7 +68,7 @@ configs.each do |db, options|
   end
 
   start_time = (Time.now - options[:raw_input][:check_window_days].days).floor
-  end_time = Time.at(camus_current.values.min).floor(1.hour) - 1.hour
+  end_time = Time.at(camus_current.values.min).floor(1.hour)
 
   puts "Scanning from #{start_time} to #{end_time}"
   puts "Max skew: #{Time.at(camus_current.values.max) - end_time}"
