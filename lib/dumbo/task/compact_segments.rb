01772e57c76b53b6787f0e213a5e16c5a1169b6c
Hagen Rother <hagen.rother@liquidm.com>
2019-09-18 12:14:21 +0200

    backport to pre 0.15

--- lib/dumbo/task/compact_segments.rb
+++ lib/dumbo/task/compact_segments.rb
@@ -12,30 +12,74 @@ module Dumbo
 
       def as_json(options = {})
         config = {
-          type: 'compact',
-          dataSource: @source['dataSource'],
-          interval: @interval,
-          dimensionsSpec: {
-            dimensions: (@source['dimensions'] || []),
-            spatialDimensions: (@source['spacialDimensions'] || []),
-          },
-          metricsSpec: (@source['metrics'] || {}).map do |name, aggregator|
-            { type: aggregator, name: name, fieldName: name }
-            # WARNING: do NOT use count for events, will count in segment vs count in raw input
-          end + [{ type: "longSum", name: "events", fieldName: "events" }],
-          segmentGranularity: @source['output']['segmentGranularity'] || "hour",
-          targetCompactionSizeBytes: 419430400,
-          tuningConfig: {
-            type: "index",
-            partitionDimensions: [@source['output']['partitionDimension']],
-            indexSpec: {
-              bitmap: {
-                type: @source['output']['bitmap'] || "roaring",
+          type: 'index_hadoop',
+          spec: {
+            dataSchema: {
+              dataSource: @source['dataSource'],
+              parser: {
+                parseSpec: {
+                  format: "json",
+                  timestampSpec: {
+                    column: (@source['input']['timestamp']['column'] || 'timestamp'),
+                    format: (@source['input']['timestamp']['format'] || 'ruby'),
+                  },
+                  dimensionsSpec: {
+                    dimensions: (@source['dimensions'] || []),
+                    spatialDimensions: (@source['spacialDimensions'] || []),
+                  }
+                }
               },
-              longEncoding: "auto",
+              metricsSpec: (@source['metrics'] || {}).map do |name, aggregator|
+                { type: aggregator, name: name, fieldName: name }
+              # WARNING: do NOT use count for events, will count in segment vs count in raw input
+              end + [{ type: "doubleSum", name: "events", fieldName: "events" }],
+              granularitySpec: {
+                segmentGranularity: @source['output']['segmentGranularity'] || "hour",
+                queryGranularity: @source['output']['queryGranularity'] || "minute",
+                intervals: [@interval],
+              }
+            },
+            ioConfig: {
+              type: 'hadoop',
+              inputSpec: {
+                type: 'dataSource',
+                ingestionSpec: {
+                  type: 'dataSource',
+                  dataSource: @source['dataSource'],
+                  interval: @interval,
+                  granularity: @source['output']['queryGranularity'] || "minute",
+                },
+              },
+            },
+            tuningConfig: {
+              type: "hadoop",
+              overwriteFiles: true,
+              ignoreInvalidRows: true,
+              buildV9Directly: true,
+              useCombiner: true,
+              forceExtendableShardSpecs: true,
+              partitionsSpec: {
+                type: "hashed",
+                targetPartitionSize: (@source['output']['targetPartitionSize'] || 419430400),
+                numShards: -1,
+              },
+              indexSpec: {
+                bitmap: {
+                  type: @source['output']['bitmap'] || "roaring",
+                },
+                longEncoding: "auto",
+              },
             },
           },
         }
+
+        if @source['output']['partitionDimension']
+          config[:spec][:tuningConfig][:partitionsSpec] = {
+             type: "dimension",
+             partitionDimension: @source['output']['partitionDimension'],
+             targetPartitionSize: (@source['output']['targetPartitionSize'] || 419430400),
+          }
+        end
 
         config
       end
