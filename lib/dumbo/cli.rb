b1444b8bc0c4428285c155dceec52fb38c0f9906
Hagen Rother <hagen.rother@liquidm.com>
2019-09-28 20:10:41 +0200

    only force a run on too few, too many possible with overlapping segments that can't be unloaded

--- lib/dumbo/cli.rb
+++ lib/dumbo/cli.rb
@@ -263,8 +263,8 @@ module Dumbo
           should_compact
         end
 
-        if source['output']['numShards'] && segment_input[:segments].size != source['output']['numShards']
-          $log.info("detected segment number mismatch,", is: segment_input[:segments].size, expected: source['output']['numShards'])
+        if source['output']['numShards'] && segment_input[:segments].size < source['output']['numShards']
+          $log.info("detected too few segments,", is: segment_input[:segments].size, expected: source['output']['numShards'])
           must_compact = true
         end
 
